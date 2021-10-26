2dc1fd71698aa695218d30c6aba70336e81d5633
Gabriela Lima <79032290+gabibarrense@users.noreply.github.com>
2021-10-25 22:52:55 -0300

    Add files via upload

---
+++ ex3.js
@@ -0,0 +1,26 @@
+//1
+var soma = 4 + 2;
+console.log(soma);
+
+//2
+var subtracao = 4 - 2;
+console.log(subtracao);
+
+//3
+var x = 8;
+var y = 4;
+console.log( x + y );
+
+//4
+var nova = x + y;
+console.log(nova);
+
+//5
+nova = "1" +1;
+console.log(nova);
+
+//6
+nova = "olá" + 23;
+console.log(nova);
+
+console.log("Tenho um filho de"+" "+ y +" "+"e outro de"+" " + soma );
